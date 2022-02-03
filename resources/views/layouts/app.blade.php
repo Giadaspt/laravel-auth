7769e1521373b4d234e0180d238e6990a7fd7071
Giadaspt <saputo.giada@gmail.com>
2022-02-03 15:28:45 +0100

    nav-bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,65 +21,48 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <span>
-                    <a class="navbar-brand" href="{{ route('home') }}">
-                        Torna al sito
-                    </a>
-                </span>
-                <span class="mr-3 ">
-                    <a href="{{ route('admin.posts.index') }}">
-                        Tutti i post
-                    </a>
-                </span>
-                <span class="mr-3">
-                    <a href="{{ route('admin.posts.create') }}">
-                        Crea il post
-                    </a>
-                </span>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
+        <nav class="navbar navbar-expand-md navbar bg-dark shadow-sm">
+            <div class="container d-flex justify-content-between">
+                <div class="d-flex align-items-center">
+                    <span>
+                        <a class="nav-link lead {{ (Route::currentRouteName() === 'home') ? 'active' : '' }}" href="{{ route('home') }}">
+                            Torna al sito
+                        </a>
+                    </span>
+                    @auth
+                        <ul class="nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link {{ (Route::currentRouteName() === 'admin.posts.index') ? 'active' : '' }}" href="{{ route('admin.posts.index') }}">Tutti i post</a>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
+                            <li class="nav-item">
+                                <a class="nav-link {{ (Route::currentRouteName() === 'admin.posts.create') ? 'active' : '' }}" href="{{ route('admin.posts.create') }}"> Crea il post </a>
                             </li>
-                        @endguest
-                    </ul>
+                        </ul>
+                    @endauth
+                </div> 
+                <div class="d-flex">
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link {{ (Route::currentRouteName() === 'login') ? 'active' : '' }}" href="{{ route('login') }}"> Login </a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item ">
+                            <a class="nav-link {{ (Route::currentRouteName() === 'register') ? 'active' : '' }}" href="{{ route('register') }}"> Register </a>
+                        </li>
+                        @endif
+                    @endguest
+
+                    @auth
+                        <a href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                        Logout
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    @endauth
                 </div>
             </div>
         </nav>
